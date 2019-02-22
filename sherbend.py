394b7d6092d844b0adbb1512aac9be2c5435e067
Daniel Pilon <bergeronpilon@gmail.com>
2019-02-22 16:29:37 -0500

    Add test file

--- sherbend.py
+++ sherbend.py
@@ -5,8 +5,27 @@
 
 import fiona
 
-import shapely
+from shapely import LineString
 
-print (46)
+# Read Options
+
+in_file = 'test.gpkg'
+with fiona.open (in_file, 'r') as source:
+    crs = source.crs
+    driver = source.driver
+    schema = source.schema
+
+    for feature in source:
+        try:
+            geom = feature['geometry']
+            properties = feature['properties']
+            type = geom['type']
+            coords = geom['coordinates']
+            if type = 'LineString':
+                shapely_feature = LineString(coords)
+
+        except Exception, e:
+            print ("Error processing feature) {0}".format(feature['id']))
+
 
 
