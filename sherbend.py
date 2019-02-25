011902ec6681bef536f09faef21ef1295e77f124
Dan-Eli <bergeronpilon@gmail.com>
2019-02-25 05:08:15 -0500

    Rename file name

--- sherbend.py
+++ sherbend.py
@@ -5,31 +5,37 @@
 
 import fiona
 
+
 from shapely.geometry import LineString
 
 # Read Options
 
-in_file = r'data\test_line.shp'
-print (fiona.listlayers(in_file))
+in_file = r'data\simple_file.gpkg'
+
+# Extract list of layers
+layers = fiona.listlayers(in_file)
+
 shapely_features = []
-with fiona.open (in_file, 'r', layer='test_line') as source:
-    crs = source.crs
-    driver = source.driver
-    schema = source.schema
+for layer_name in layers:
+    with fiona.open(in_file, 'r', layer=layer_name) as source:
+        crs = source.crs
+        driver = source.driver
+        schema = source.schema
 
-    for feature in source:
-        try:
-            geom = feature['geometry']
-            properties = feature['properties']
-            type = geom['type']
-            coords = geom['coordinates']
-            if type == 'LineString':
-                shapely_feature = LineString(coords)
-                shapely_feature.properties = properties
-                shapely_features.append(shapaly_feature)
+        for feature in source:
+            try:
+                geom = feature['geometry']
+                properties = feature['properties']
+                type = geom['type']
+                coords = geom['coordinates']
+                if type == 'LineString':
+                    shapely_feature = LineString(coords)
+                    shapely_feature.properties = properties
+                    shapely_features.append(shapely_feature)
 
-        except:
-            print ("Error processing feature) {0}".format(feature['id']))
+            except:
+                print ("Error processing feature) {0}".format(feature['id']))
+    source.close()
 
 
 
