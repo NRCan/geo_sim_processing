1fb0005a7893fb15d242c72dc985e8f829ef442c
Dan-Eli <bergeronpilon@gmail.com>
2019-02-27 22:49:19 -0500

    Code for read/write file

--- sherbend.py
+++ sherbend.py
@@ -11,67 +11,109 @@ from shapely.geometry import LineString
 
 @dataclass
 class Command:
-    in_file: str # Name of the input file
-    out_file: str # Name of the output file
-    first_last: bool # Flag to simplify first last vertice
-    tolerance: float # Tolerance of simplification
-    simplicity: bool # Flag to validate the simple feature
-    sidedness: bool # Flag to validate the sidedness
-    crossing: bool # Flag to validate the crossing
-    connection: bool # Flag to verify the connection
+    """Contains the parameters of the command.
+
+        Keyword arguments:
+        in_file -- name of the input file
+        out_file -- name of the output file
+        first_last -- flag to simplify the first/last bend
+        simplicity -- flag to test for OGC simple line constraint
+        adjacency/sidedness/consistency -- flag to test for consistency constraint
+        connection -- flag to test connection constraint
+
+        """
+    in_file: str
+    out_file: str
+    first_last: bool
+    tolerance: float
+    simplicity: bool
+    sidedness: bool
+    crossing: bool
+    connection: bool
 
 
 @dataclass
 class Layer:
-    name: str # Name of the layer
-    type: str # Type of feature
-    crs: dict # Dictionary of the coordinate reference system
-    schema: dict # Schema dictionary of the layer
-    features: List[object]=None # List of features
+    """Contains the parameters of a layer.
+
+        Keyword arguments:
+        name -- name of the layer
+        type -- geometry type
+        schema -- layer schema
+        features -- list of geographic features
+
+        """
+    name: str
+    type: str
+    schema: dict
+    features: List[object]=None
 
 
 @dataclass
 class Params:
-    command: Command
-    layers: List[Layer]=None
+    """Contains the parameters .
 
-command = Command(in_file='', out_file='', first_last=True, tolerance=10., simplicity=True,
+            Keyword arguments:
+            command -- parameter of the command line
+            crs -- coordinate reference system
+            driver -- layer schema
+            features -- list of geographic features
+
+            """
+    command: Command
+    crs: str
+    driver: str
+
+
+placer le nom du layer dans le feature shapely  et créer un dict de schema
+à l'écriture extraire tous les feature avec schemas identique et faire l'écriture dans le fichier par couche
+
+command = Command (in_file='', out_file='', first_last=True, tolerance=10., simplicity=True,
                   sidedness=True, crossing=True, connection=True)
-params = Params(command=command, layers=[])
+params = Params (command=command, crs=None, driver=None)
 
 params.command.in_file = r'data\simple_file.gpkg'
-params.command.in_file = r'data\simple_file_out.gpkg'
+params.command.out_file = r'data\simple_file_out.gpkg'
 
 in_file = params.command.in_file
 
 
 # Extract and load the layers of the file
-layers = fiona.listlayers(in_file)
-shapely_features = []
-for layer_name in layers:
+layer_names = fiona.listlayers(in_file)
+features = []
+for layer_name in layer_names:
     with fiona.open(in_file, 'r', layer=layer_name) as source:
-        crs = source.crs
-        driver = source.driver
+        params.crs = source.crs
+        params.driver = source.driver
         schema = source.schema
 
-        for feature in source:
+        for in_feature in source:
             try:
-                geom = feature['geometry']
-                properties = feature['properties']
-                type = geom['type']
-                coords = geom['coordinates']
-                if type == 'LineString':
-                    shapely_feature = LineString(coords)
-                    shapely_feature.properties = properties
-                    shapely_features.append(shapely_feature)
+                geom = in_feature['geometry']
+                if geom['type'] == 'LineString':
+                    feature = LineString(geom['coordinates'])
+                    feature.properties = in_feature['properties']
+                    features.append(feature)
 
             except:
                 print ("Error processing feature) {0}".format(feature['id']))
-        layer = Layer(name=layer_name, type=type, crs=source.crs, schema=source.schema, features=shapely_features)
+        layer = Layer(name=layer_name, type=type, schema=source.schema, shapely_features=features)
     params.layers.append(layer)
     source.close()
 
-print (1)
+for layer in params.layers:
+    with fiona.open(params.command.out_file, 'w',
+                    driver=params.driver,
+                    layer=layer.name,
+                    crs=params.crs,
+                    schema=schema) as dest:
+        for feature in layer.features:
+            out_feature = {'geometry': {'type': layer.type,
+                                        'coordinates': list(feature.coords) },
+                            'properties': feature.properties }
+            dest.write(out_feature)
+
+        dest.close()
 
 
 
